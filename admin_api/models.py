cf86854150d17bf2f8100be898439fcde45bde28
Lyhav <Lyhav3930@gmail.com>
2024-05-31 01:39:45 +0700

    fix( attendance status)

--- admin_api/models.py
+++ admin_api/models.py
@@ -83,7 +83,7 @@ class Student(models.Model):
 class Attendance(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     date = models.DateField()
-    status = models.CharField(max_length=10, default="AP")
+    status = models.CharField(max_length=10, default="P")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
     def __str__(self):
